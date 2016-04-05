480b76755a1f44b272be00d84b8eb4c0fda52ff7
Steve McCauley <steven_mccauley@brown.edu>
2016-04-05 14:42:39 -0400

    Prepare schema

--- chords/db/schema.sql
+++ chords/db/schema.sql
@@ -4,21 +4,44 @@ drop table if exists faculty;
     shortid text not null,
     firstname text not null,
     lastname text not null,
+    fullname text not null,
     preftitle text not null,    
-    email text not null
+    email text not null,
+    primarydept text not null,
+    FOREIGN KEY(primarydept) REFERENCES department(rabid)
 );
 
-drop table if exists visualizations;
-    create table visualizations (
-    id integer primary key autoincrement,
-    vizjson text not null,
+drop table if exists department;
+    create table department (
+    rabid text primary key,
+    label text not null
 );
 
-drop table if exists facultyVizAssoc;
-    create table facultyVizAssoc (
+drop table if exists chord_department;
+    create table chord_department (
     id integer primary key autoincrement,
-    facid integer not null,
-    vizid integer not null,
-    FOREIGN KEY(facid) REFERENCES faculty(rabid),
-    FOREIGN KEY(vizid) REFERENCES visualizations(id),
+    deptid text not null,
+    deptlabel text not null,
+    facultykey text not null,
+    facultydata text not null,
+    FOREIGN KEY(deptid) REFERENCES department(rabid)
+);
+
+drop table if exists chord_faculty;
+    create table chord_faculty (
+    id integer primary key autoincrement,
+    facultyid text not null,
+    key text not null,
+    data text not null,
+    FOREIGN KEY(facultyid) REFERENCES faculty(rabid),
+);
+
+drop table if exists force_department;
+    create table force_department (
+    id integer primary key autoincrement,
+    deptid text not null,
+    deptlabel text not null,
+    nodes text not null,
+    links text not null,
+    FOREIGN KEY(deptid) REFERENCES department(rabid)
 );
