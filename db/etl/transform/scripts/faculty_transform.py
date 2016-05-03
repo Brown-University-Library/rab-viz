0505432faa2ae03d4aae8b4377029777306107ea
Steve McCauley <steven_mccauley@brown.edu>
2016-05-03 13:46:27 -0400

    Faculty transform script expects OU data from VIVO

--- db/etl/transform/scripts/faculty_transform.py
+++ db/etl/transform/scripts/faculty_transform.py
@@ -3,70 +3,33 @@ import csv
 import os
 import json
 
-def main(inFileFac, trnsDept,
-			trnsAffs, targetDir):
+def main(inFileFac, targetDir):
 	if not os.path.exists(targetDir):
 		os.makedirs(targetDir)
 
-	depts = dict()
-	affs = dict()
 	facs = []
-
-	with open(trnsDept, "r") as f:
-		rdr = csv.reader(f, delimiter=',', quotechar='"')
-		# deptid, label
-		for row in rdr:
-			depts[row[0]] = row[1]
 
 	with open(inFileFac, "r") as f:
 		rdr = csv.reader(f, delimiter=',', quotechar='"')
 		#Skip header
 		head = rdr.next()
-		# rabid, last, first, label, title 
+		# rabid, last, first, label, title, primaryOU 
 		for row in rdr:
-			facs.append((row[0], row[1], row[2], row[3], row[4]))
-
-	with open(trnsAffs, "r") as f:
-		#####!!!!!!!!!!!!!!!!!!!!!!!!!! ##########
-		##### ALL OF THIS IS IRRELEVANT ##########
-		##### FIX IN FIS !!!!!!!!!!!!!! ##########
-		#####!!!!!!!!!!!!!!!!!!!!!!!!!! ##########
-
-		rdr = csv.reader(f, delimiter=',', quotechar='"')
-		# facid, deptid, rank(?)
-		for row in rdr:
-			# Only interested in positions ranked 1
-			# This also skips unranked positions (val=0)
-			if row[2] != '1':
-				continue
-			if not affs.get(row[0]):
-				# no affiliations associated with the rabid
-				# in affs dict, so...
-				if depts.get(row[1]):
-					# lookup deptid from affiliation in dept dict...
-					affs[row[0]] = depts[row[1]]
-				else:
-					# if there is no corresponding entry (ie, a
-					# dept that no longer exists), list "Other"
-					affs[row[0]] = "Other"
+			if not row[5]:
+				primaryOU = "None"
 			else:
-				# If there is already a value in the affs dict,
-				# that means there are multiple affs for this rabid
-				affs[row[0]] = "Multiple"
+				primaryOU = row[5]
+			facs.append((row[0], row[1], row[2], row[3], row[4], primaryOU))
 
 	with open(
 			os.path.join(targetDir,'faculty_data.csv'),
 			 'w') as dataout:
 		wrtr = csv.writer(dataout)
 		for fac in facs:
-			rabid, last, first, name, title = fac
+			rabid, last, first, name, title, ou = fac
 			abbrev = last + ", " + first[0]
-			if affs.get(rabid):
-				deptLabel = affs[rabid]
-			else:
-				deptLabel = "None"
-			row = (rabid, last, first, name, abbrev, title, deptLabel)
+			row = (rabid, last, first, name, abbrev, title, ou)
 			wrtr.writerow(row)
 
 if __name__ == "__main__":
-	main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+	main(sys.argv[1], sys.argv[2])
