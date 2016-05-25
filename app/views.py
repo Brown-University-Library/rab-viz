0b1a9d807576d2e3e8c2fd4432e5d58480bc5c52
Steve McCauley <steven_mccauley@brown.edu>
2016-05-25 16:12:44 -0400

    Add bug icon to legend. No longer rely on d3 color scale

--- app/views.py
+++ app/views.py
@@ -3,6 +3,37 @@ from flask import render_template, json
 from app import app
 from .models import ChordViz, ForceViz, Faculty, Departments
 
+colorRange = ['rgb(23,190,207)','rgb(188,189,34)','rgb(227,119,194)',
+'rgb(148,103,189)','rgb(214,39,40)','rgb(44,160,44)','rgb(255,127,14)',
+'rgb(31,119,180)','rgb(214,97,107)','rgb(206,109,189)','rgb(214,97,107)',
+'rgb(231,186,82)','rgb(181,207,107)','rgb(107,110,207)','rgb(230,85,13)',
+'rgb(49,130,189)','rgb(49,163,84)','rgb(158,154,200)','rgb(253,141,60)',
+'rgb(116,196,118)','rgb(189,158,57)']
+
+# colorRange = [
+# 			"rgba(23,190,207,1)",
+# 			"rgba(188,189,34,1)",
+# 			"rgba(227,119,194)",
+# 			"rgb(148,103,189,1)",
+# 			"rgba(214,39,40,1)",
+# 			"rgba(44,160,44,1)",
+# 			"rgba(255,127,14,1)",
+# 			"rgba(31,119,180,1)",
+# 			"rgba(214,97,107,1)",
+# 			"rgba(206,109,189,1)",
+# 			"rgba(214,97,107,1)",
+# 			"rgba(231,186,82,1)",
+# 			"rgba(181,207,107,1)",
+# 			"rgba(107,110,207,1)",
+# 			"rgba(230,85,13,1)",
+# 			"rgba(49,130,189,1)",
+# 			"rgba(49,163,84,1)",
+# 			"rgba(158,154,200,1)",
+# 			"rgba(253,141,60,1)",
+# 			"rgba(116,196,118,1)",
+# 			"rgba(189,158,57,1)"
+# 			]
+
 @app.route('/chord/<viztype>/<rabid>')
 @app.route('/chord/<viztype>/<rabid>/<page>')
 def chordViz(viztype, rabid, page=0):
@@ -48,4 +79,4 @@ def forceViz(viztype, rabid, page=0):
 	return render_template(
 			'force.html', pageLabel=pageLabel, legend=deptList,
 			deptMap=deptMap, facMap=facMap, vizdata=vizdata,
-			linkDist=30, repel=-350)
+			linkDist=30, repel=-350, crange=colorRange)
