177e9c65d8c50ea98e40a9c3098ae55f61e8d152
Steven McCauley <steven_mccauley@brown.edu>
2016-07-08 15:34:03 -0400

    Ignore config file; add config template

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db/visualizations.db')
-VIVO_DATA_SERVICE='https://vivo.brown.edu/services/data/v1/'
-VIVO_GRAPH_SERVICE='http://localhost:8000/'
---
+++ config_template.py
@@ -0,0 +1,6 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = ''
+VIVO_DATA_SERVICE=''
+VIVO_GRAPH_SERVICE=''
