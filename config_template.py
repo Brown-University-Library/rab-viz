3e0dc32b59e12f64b9ba13a7d94b227511774f21
Steven McCauley <Steven_McCauley@brown.edu>
2018-01-23 15:34:27 -0500

    Restructure app

--- config_template.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = ''
-VIVO_URL = ''
-VIVO_DATA_SERVICE=''
-VIVO_GRAPH_SERVICE=''
--- local-env_template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Expecting Fuseki.
-export VIVO_ENDPOINT=''
-export DATA_NAMESPACE='http://vivo.brown.edu/individual/'
-export VIVO_DATA_SERVICE='https://vivo.brown.edu/services/data/v1/'
-export VIVO_GRAPH_SERVICE=''
