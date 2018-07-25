a3b3a45f2f336745ec04de2aa18dd4fff23597bf
Steven McCauley <steven_mccauley@brown.edu>
2018-07-25 13:59:49 -0400

    Capture collaborator data

---
+++ rabviz/etl/extract/collaborators.py
@@ -0,0 +1,20 @@
+from etl.validate import validate_data, validate_dataset
+
+query = """
+    PREFIX vivo:     <http://vivoweb.org/ontology/core#>
+    SELECT ?faculty ?collaborator
+    WHERE
+    {
+          ?faculty vivo:hasCollaborator ?collaborator. 
+    }
+    """
+
+destination = "collaborators.csv"
+
+
+def validate(rows):
+    test_head = [ 'faculty', 'collaborator' ]
+    validated = validate_dataset.header(rows, test_head)
+    validated = [ validate_data.shortid_uri(row, 0) for row in validated ]
+    validated = [ validate_data.shortid_uri(row, 1) for row in validated ]
+    return validated
---
+++ rabviz/etl/transform/collaborator_graph.py
@@ -0,0 +1,61 @@
+import networkx
+from transform import utils
+
+collection_name = 'collaborators'
+key_field = 'rabid'
+value_field = 'graph'
+input_files = ['faculty.csv', 'collaborators.csv']
+
+def unique_on_fields(data, fields=[]):
+    wrapped = [ (frozenset([ row[field] for field in fields ]), row)
+                    for row in data ]
+    checked = set()
+    filtered = []
+    for w in wrapped:
+        if w[0] in checked:
+            continue
+        else:
+            filtered.append(w[1])
+            checked.add(w[0])
+    return filtered
+
+def build_total_graph(data):
+    graph = networkx.Graph()
+    unique_data = unique_on_fields(data, [0,1])
+    for row in unique_data:
+        if graph.has_edge(row[0], row[1]):
+            graph[row[0]][row[1]]['weight'] += 1
+        else:
+            graph.add_edge(row[0],row[1], weight=1)
+    return graph
+
+def get_subgraph_by_node(graph, node, depth=2):
+    nodes = { node }
+    for d in range(depth):
+        for n in nodes:
+            neighbors = { b for b in iter(graph[n]) }
+            nodes = nodes | neighbors
+    subgraph = networkx.Graph(graph.subgraph(nodes))
+    return subgraph
+
+def add_node_attributes(graph, node_attribute_lookup):
+    nodes = [ n for n in graph.nodes() ]
+    nodes_with_attrs = [ node_attribute_lookup[n] for n in nodes ]
+    for n in nodes_with_attrs:
+        graph.add_node(n[0], name=n[1], group=n[2], title=n[3])
+    return graph
+
+def key_graph_by_node(node, graph):
+    subgraph = get_subgraph_by_node(graph, node)
+    data = networkx.node_link_data(subgraph)
+    return data
+
+def transform(facultyData, collaboratorData):
+    faculty_attrs = [ utils.row_reducer(row, [0,3,5,4]) for row in facultyData ]
+    faculty_index = utils.data_indexer(faculty_attrs, 0)
+    collab_graph = build_total_graph(collaboratorData)
+    graph_with_attrs = add_node_attributes(collab_graph, faculty_index)
+    faculty_list = [ n for n in graph_with_attrs.nodes() ]
+    gnrtr =  utils.data_generator(
+        faculty_list, graph_with_attrs, key_graph_by_node)
+    return gnrtr
